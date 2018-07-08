3b5c8aea3c7cc66daad5e4ab63ba3f9585532091
Dekk <dekkservidores@gmail.com>
2018-07-08 14:22:32 -0500

    Inicio del proyecto

---
+++ vistas/plantilla.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="tutle" content="Tienda virtual">
+    <meta name="description" content="Esto es una pagina de ecomerce tienda virtual">
+    <title>Tienda Virtual</title>
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
+    <link rel="stylesheet" href="vistas/css/plantilla.css">
+    <link rel="stylesheet" href="vistas/css/cabezote.css">
+
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <?php
+    //HEADER
+    include "modulos/cabezote.php";
+    ?>
+
+    <script src="vistas/js/cabezote.js"></script>
+  </body>
+</html>
